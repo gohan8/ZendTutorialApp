e48ea97550bdaf571ed36588163be37b22119ba0
gohan8 <thiagorsf8@gmx.com>
2018-04-15 17:52:51 +0000

    Still adjusting database creation scripts.

--- scripts/createDb.php
+++ scripts/createDb.php
@@ -1,31 +1,42 @@
 <?php
 echo __DIR__;
 
-$configFile = __DIR__ . '/../config/local.ini';
+$configFile = __DIR__ . '/../config/loca.ini';
 $sqlScriptFile = __DIR__ . '/../data/createDb.sql';
-$configSessions = parse_ini_file($configFile,true);
-print_r($configSessions);
 
-#convert sessions to dot notation [session] var = "value" to array ["session.var" => "value"]
-foreach ($configSessions as $name=>$value) {
-	if (is_array($value)) {
-		foreach ($value as $n=>$v) $config["$name.$n"] = $v;
-	}else{
-		$config[$name]=$value;
-	}
+$configSessions = parse_ini_file($configFile,true);
+if ($configSessions === false) {
+	error_log("Unable to get configuration from $configFile.\nUsing defaults.");
+	$config = ['db.host'=>'localhost', 'db.port'=>3306, 'db.user' => '', 'db.passwd' => '' ];
+} else {
+
+    #convert sessions to dot notation [session] var = "value" to array ["session.var" => "value"]
+    foreach ($configSessions as $name=>$value) {
+    	if (is_array($value)) {
+    		foreach ($value as $n=>$v) $config["$name.$n"] = $v;
+    	}else{
+    		$config[$name]=$value;
+    	}
+    }
 }
 unset($configSessions);
-print_r($config);
 
-$dsn = "mysql:host=${config['db.host']};port=${config['db.port']}";
-$dsn = "$dsn;dbname=${config['db.database']};charset=utf8;";
+
+$dsn = "mysql: host=${config['db.host']}; port=${config['db.port']}";
+if (array_key_exists('db.database', $config)) 
+    $dsn = "$dsn; dbname=${config['db.database']};";
+$dsn = "$dsn charset=utf8;";
 
 $sqlStatements = file_get_contents($sqlScriptFile);
+if ($sqlStatements === false) {
+    $errMsg = "Unable to read sql script. [$sqlScriptFile]\n Test DataBase not created.";
+    error_log($errMsg);
+    die($errMsg);
+}
 try {
 	$dbCon = new PDO($dsn, $config['db.user'], $config['db.passwd']);
-	$count = $dbcon->exec($sqlStatements);
+	$count = $dbCon->exec($sqlStatements);
 	print "Sql create script returns $count \n";
-	$dbCon->close;
 }catch(PDOException $pdoe){
 	error_log($pdoe->getMessage());
 	error_log($pdoe->getTraceAsString());
